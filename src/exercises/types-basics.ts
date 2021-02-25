2dd63c864d4126245049fadf536a86cf2a63ce67
iOznny <fo170014@gmail.com>
2021-02-24 20:51:44 -0600

    avance

---
+++ src/exercises/types-basics.ts
@@ -0,0 +1,12 @@
+console.log('Hola Mundo!');
+
+/* Código de TypeScript */
+
+let names: string = 'Angular';
+let hp: number | string = 95;
+let vivo: boolean = true;
+
+hp = 'full';
+
+console.log(names, hp);
+
